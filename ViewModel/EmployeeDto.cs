949c5af0fef22a288efc78bfeea64caa1532a77f
gbeydtha <beydtha@gmail.com>
2025-04-04 20:15:26 +0200

    update dto class and view

--- ViewModel/EmployeeDto.cs
+++ ViewModel/EmployeeDto.cs
@@ -6,16 +6,16 @@ namespace DruckEmployeeMvc.ViewModel
     {        
     
         [Required]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [Required]
-        public string? Position { get; set; }
+        public string Position { get; set; }
 
         [Required]
-        public string? Department { get; set; }   
+        public string Department { get; set; }   
 
         [Required]
-        public string? Email { get; set; }
+        public string Email { get; set; }
  
     }
 }
